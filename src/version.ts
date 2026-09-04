79b8c8e1342c240d4db91430712dc35e45edea97
chan <chan@eldis24.ru>
2026-09-04 16:24:42 +0300

    fix runtime package version lookup

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,8 @@
 import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
-const packageJson = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), 'utf8')) as { version: string };
+const packageJson = JSON.parse(readFileSync(resolve(process.cwd(), 'package.json'), 'utf8')) as {
+	version: string;
+};
+
 export const VERSION = packageJson.version;
